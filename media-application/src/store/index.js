caa947498afbbbd243add46f82686d2288578c18
jesusoyer <oyervidesjesus017@gmail.com>
2023-06-06 14:05:06 -0500

    finished project finished tags and and fetch, post, and delete CRUD operations to the photos API

--- media-application/src/store/index.js
+++ media-application/src/store/index.js
@@ -2,15 +2,18 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { usersReducer  } from './slices/usersSlice';
 import { albumsApi } from './apis/albumsApi';
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore ({
     reducer: {
         users: usersReducer,
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
         .concat(albumsApi.middleware)
+        .concat(photosApi.middleware)
     }
 });
 
@@ -22,3 +25,4 @@ export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
 export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumsApi';
+export { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } from './apis/photosApi';
